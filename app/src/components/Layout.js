eba30fdb34bc6f5b2078c5f968dcef593625f7d3
unknown <mendozapaulo89@gmail.com>
2023-10-04 12:14:05 +0800

    first commit

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -38,7 +38,7 @@ export default function Layout(props) {
   return(
     <div className={gridStyle}>
       <section className={headerStyle}>
-        <Header title="Sample Blog"/>
+        <Header title="Jose Paulo E. Mendoza Blog"/>
       </section>
       <Navigation className={sideNavStyle} />
 
